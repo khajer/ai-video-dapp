b8d367a12e3e98538bf86b65393bdd0f7b8a559c
itsara <khajer@gmail.com>
2025-05-16 17:12:13 +0700

    set explore draft

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
-import { DashboardFeature } from '@/components/dashboard/dashboard-feature'
+import { SideBar } from '@/components/dashboard/side-bar'
 
 export default function Home() {
-  return <DashboardFeature />
+  return (
+    <div className="flex min-h-screen">
+      {/* Sidebar */}
+      <aside className="w-64 bg-gray-900 text-white p-4">
+        <SideBar />
+      </aside>
+
+      {/* Main Content */}
+      <main className="flex-1 bg-gray-100 p-4 overflow-auto bg-gray-800">
+        <div>Explore</div>
+      </main>
+    </div>
+  )
 }

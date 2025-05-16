acb89d3806484b5311f41f4c4f60200a53f8cd97
itsara <khajer@gmail.com>
2025-05-16 17:26:03 +0700

    fix bug header menu list (unused)

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -10,9 +10,9 @@ export function AppHeader() {
   const pathname = usePathname()
   const [showMenu, setShowMenu] = useState(false)
 
-  function isActive(path: string) {
-    return path === '/' ? pathname === '/' : pathname.startsWith(path)
-  }
+  // function isActive(path: string) {
+  //   return path === '/' ? pathname === '/' : pathname.startsWith(path)
+  // }
 
   return (
     <header className="relative z-50 px-4 py-2 bg-neutral-100 dark:bg-neutral-900 dark:text-neutral-400">
@@ -34,19 +34,6 @@ export function AppHeader() {
         {showMenu && (
           <div className="md:hidden fixed inset-x-0 top-[52px] bottom-0 bg-neutral-100/95 dark:bg-neutral-900/95 backdrop-blur-sm">
             <div className="flex flex-col p-4 gap-4 border-t dark:border-neutral-800">
-              <ul className="flex flex-col gap-4">
-                {links.map(({ label, path }) => (
-                  <li key={path}>
-                    <Link
-                      className={`hover:text-neutral-500 dark:hover:text-white block text-lg py-2  ${isActive(path) ? 'text-neutral-500 dark:text-white' : ''} `}
-                      href={path}
-                      onClick={() => setShowMenu(false)}
-                    >
-                      {label}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
               <div className="flex flex-col gap-4">
                 <WalletButton />
               </div>

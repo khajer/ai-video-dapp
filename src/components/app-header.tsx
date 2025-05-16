162951f4dee4d71d2c056d8dbb7f289b30e7715b
itsara <khajer@gmail.com>
2025-05-17 02:06:25 +0700

    add icon user

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import { Menu, X } from 'lucide-react'
 import { WalletButton } from '@/components/solana/solana-provider'
+import { CircleUser } from 'lucide-react'
 
 export function AppHeader() {
   // const pathname = usePathname()
@@ -22,15 +23,13 @@ export function AppHeader() {
             <span>ShotVDO</span>
           </Link>
         </div>
-
         <Button variant="ghost" size="icon" className="md:hidden" onClick={() => setShowMenu(!showMenu)}>
           {showMenu ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
         </Button>
-
         <div className="hidden md:flex items-center gap-4">
           <WalletButton size="sm" />
+          <CircleUser />
         </div>
-
         {showMenu && (
           <div className="md:hidden fixed inset-x-0 top-[52px] bottom-0 bg-neutral-100/95 dark:bg-neutral-900/95 backdrop-blur-sm">
             <div className="flex flex-col p-4 gap-4 border-t dark:border-neutral-800">
